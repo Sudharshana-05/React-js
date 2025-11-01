d21198a5585561a7cdf08dc4b70287d2164c9700
Sudharshana-05 <sudharshana2k2@gmail.com>
2025-11-01 12:17:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+
+import HTML from './assets/html.png';
+import CSS from './assets/CSS-Logo-2011.png';
+import JS from './assets/JS.png';
+import Course from './course';
+import './App.css';
+function App() {
+  return (
+    <>
+   <Course   name="html" price="199" image={HTML}  rating ={23}/>
+    <Course name="Css" price="100" image={CSS} rating={1} show={true} />
+    <Course image={JS} rating={2} show={true}/>
+    </>  
+  );
+}
+
+export default App;
---
+++ course.jsx
@@ -0,0 +1,23 @@
+
+
+function Course(props){
+    if(props.show==true){
+    return (       
+        <div className="card">
+        <img src={props.image} alt=""/>
+        <h3>{props.name}</h3>
+        <h3>{props.price}</h3>
+        <h3>{props.rating}</h3>
+        <span>{props.true}</span>
+        </div>
+    );
+}
+else{
+    return(
+        <div className="card">No courses are available</div>
+    )
+}
+}
+
+export default Course;
+
